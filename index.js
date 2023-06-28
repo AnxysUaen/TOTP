db515bd9cf6f3acb17285216d6cd7b9f7faa3f6c
AnxysUaen <AnxysUaen@Gmail.com>
2023-06-28 19:23:44 +0800

    show

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-import { URI } from "otpauth"
-// import { TOTP, URI } from "otpauth"
+const OTPAuth = require("./otpauth.esm")
+// import * as OTPAuth from "./otpauth.esm"
 
-// const TOTP_FROM_EDIT = new TOTP({
-//     issuer: "ACME",
-//     label: "AzureDiamond",
-//     algorithm: "SHA512",
-//     digits: 6,
-//     period: 60,
-//     secret: "V6YZWJLHVDVV3VRYBOUBDCZRQD4CM2YT3TMEKQ33XYMJO4JS3ICT4ZG3", // or 'OTPAuth.Secret.fromBase32("NB2W45DFOIZA")'
-// })
-// console.log(URI.stringify(TOTP_FROM_EDIT))
+const TOTP_FROM_EDIT = new OTPAuth.TOTP({
+    issuer: "ACME",
+    label: "AzureDiamond",
+    algorithm: "SHA512",
+    digits: 6,
+    period: 60,
+    secret: "V6YZWJLHVDVV3VRYBOUBDCZRQD4CM2YT3TMEKQ33XYMJO4JS3ICT4ZG3"
+})
+
+const TOTP_FROM_URI = OTPAuth.URI.parse('otpauth://totp/wanghe06000@KAYAKWISE.COM:wanghe06000_OTP?digits=6&secret=V6YZWJLHVDVV3VRYBOUBDCZRQD4CM2YT3TMEKQ33XYMJO4JS3ICT4ZG3&period=60&algorithm=SHA512&issuer=wanghe06000%40KAYAKWISE.COM')
 
 function ShowList() {
-    const TOTP_FROM_URI = URI.parse('otpauth://totp/wanghe06000@KAYAKWISE.COM:wanghe06000_OTP?digits=6&secret=V6YZWJLHVDVV3VRYBOUBDCZRQD4CM2YT3TMEKQ33XYMJO4JS3ICT4ZG3&period=60&algorithm=SHA512&issuer=wanghe06000%40KAYAKWISE.COM')
-    const token = TOTP_FROM_URI.generate()
+    let token = TOTP_FROM_EDIT.generate()
+    let token2 = TOTP_FROM_URI.generate()
 
     return {
-        title: TOTP_FROM_URI.issuer,
-        description: token,
-        icon: 'logo.png'
+        title: token,
+        description: `${TOTP_FROM_URI.issuer}--tk2=${token2}`
     }
 }
+
+// console.log([
+//     ShowList()
+// ])
 
 window.exports = {
     otp_list: {
         mode: "list",
         args: {
             enter: (action, callbackSetList) => {
-                callbackSetList([
+                let list = [
                     ShowList()
-                ])
+                ]
+                callbackSetList(list)
             },
             // 子输入框内容变化时被调用 可选 (未设置则无搜索)
             // search: (action, searchWord, callbackSetList) => {
@@ -45,12 +50,12 @@ window.exports = {
             //     ])
             // },
             // 用户选择列表中某个条目时被调用
-            // select: (action, itemData, callbackSetList) => {
-            //     window.utools.hideMainWindow()
-            //     const url = itemData.url
-            //     require('electron').shell.openExternal(url)
-            //     window.utools.outPlugin()
-            // },
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                const code = itemData.title
+                utools.copyText(code)
+                window.utools.outPlugin()
+            }
             // placeholder: "搜索"
         }
     }
